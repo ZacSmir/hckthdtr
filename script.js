12eb88c4e9b3bf789afe257e546549aaff8ba04c
ZacSmir <Zach@smirin.us>
2016-12-02 08:42:22 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,16 @@
+var editor = $(parent.document.body)
+
+editor.find("div").css({background:"black"})
+var topbar = $(parent.parent.document.body).find("#topbar")
+
+topbar.append("<div>HI THERE</div>")
+//
+var allDivsInTheEditor = editor.find("div")
+
+
+
+// Now try to
+// 1. add a button to the topbar
+// 2. change the title above the file list
+// 3. change some borders in the editor
+// 4. Make all this happen when a button is clicked
