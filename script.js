f78e728b5d127c587030d59a3740d471ec26bea8
ZacSmir <Zach@smirin.us>
2016-12-02 09:05:56 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var editor = $(parent.document.body)
 editor.find("div").css({background:"black"})
 var topbar = $(parent.parent.document.body).find("#topbar")
 topbar.css({background: "grey"})
-topbar.append("<div>HI THERE</div>")
+topbar.append("<div></div>")
 //
-var nukerussia = $(parent.parent.document.body).find(".topbar-button:nth-child(4)")
-nukerussia.html("")
+var nukerussia = $(parent.parent.document.body).find("#topbar:nth-child(4)")
+nukerussia.html("CLICK THIS TO NUKE RUSSIA")
 var allDivsInTheEditor = editor.find("div")
 
 
